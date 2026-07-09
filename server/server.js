d718ba8e5733037a7d935e2b13f26fc65debdf88
Omar Almuhtaseb <oalmuhtaeb945@gmail.com>
2026-07-09 13:36:33 +0300

    added "api" b4

--- server/server.js
+++ server/server.js
@@ -10,9 +10,9 @@ const authRoutes = require("./routes/authRoutes");
 const slotRoutes= require("./routes/slotRoutes");
 connectDB();
 
-app.use("/users", userRoutes);
-app.use("/cars", carRoutes);
-app.use("/slot",slotRoutes);
+app.use("/api/users", userRoutes);
+app.use("/api/cars", carRoutes);
+app.use("/api/slot",slotRoutes);
 app.use("/api/auth",authRoutes);
 app.listen(3000, () => {
     console.log("Server is running on http://localhost:3000");
